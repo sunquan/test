91f8e7b76fe833d41a44190e2529fccecf2dfa82
sunquan <sunnauq@qq.com>
2016-07-04 22:29:21 +0800

    add

---
+++ spring/aop/src/main/java/sun/quan/test/spring/aop/Sleepable.java
@@ -0,0 +1,7 @@
+package sun.quan.test.spring.aop;
+
+/**
+ * Created by sunquan on 16/7/4.
+ */
+public interface Sleepable {
+}
---
+++ spring/aop/src/test/java/sun/quan/test/spring/aop/test/ProxyTest.java
@@ -0,0 +1,9 @@
+package sun.quan.test.spring.aop.test;
+
+/**
+ * Created by sunquan on 16/7/4.
+ */
+
+
+public class ProxyTest {
+}
